606de6e47c67ff6e70fb200ab961d5fff143bceb
Kirill Pavlov <kirill-pavlov3105@mail.ru>
2024-12-03 23:54:57 +0300

    Update structs

--- models/booking.go
+++ models/booking.go
@@ -5,14 +5,12 @@ import "time"
 // Booking defines the structure for a booking API
 // Определяет структуру для API бронирования
 type Booking struct {
-	ID           int       `gorm:"primaryKey" json:"id"`
-	ClientID     int       `json:"client_id"`
-	HotelID      int       `json:"hotel_id"`
-	DateStart    time.Time `json:"date_start"`
-	DateEnd      time.Time `json:"date_end"`
-	RoomCategory string    `json:"room_category"`
-	RoomCount    int       `json:"room_count"`
-	Price        int       `json:"price"`
-	GuestAmount  int       `json:"guest_amount"`
-	IsCancelled  bool      `json:"is_cancelled"`
+	BookingID   int       `gorm:"primaryKey" json:"booking_id"`
+	ClientID    int       `json:"client_id"`
+	HotelID     int       `json:"hotel_id"`
+	DateStart   time.Time `json:"date_start"`
+	DateEnd     time.Time `json:"date_end"`
+	Price       int       `json:"price"`
+	GuestAmount int       `json:"guest_amount"`
+	IsCancelled bool      `json:"is_cancelled"`
 }
--- models/hotelier.go
+++ models/hotelier.go
@@ -1,12 +1,15 @@
 package models
 
 // Hotelier defines the structure for a hotelier API
+// Определяет структуру для API владельца
 type Hotelier struct {
-	ID        int    `gorm:"primaryKey" json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Login     string `gorm:"uniqueIndex" json:"login"`
-	Password  string `json:"password"`
-	HotelsId  int    `json:"hotels_id"`
-	CreatedOn string `json:"-"`
+	HotelierID int    `gorm:"primaryKey" json:"hotelier_id"`
+	HotelID    int    `gorm:"uniqueIndex" json:"hotel_id"`
+	FirstName  string `json:"first_name"`
+	LastName   string `json:"last_name"`
+	Email      string `gorm:"uniqueIndex" json:"email"`
+	Phone      string `gorm:"uniqueIndex" json:"phone"`
+	Login      string `gorm:"uniqueIndex" json:"login"`
+	Password   string `json:"password"`
+	HotelsId   int    `json:"hotels_id"`
 }
